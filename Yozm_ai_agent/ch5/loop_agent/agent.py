8cc4fc67fdde0872c4de0d0fc11b29152370f6ad
Dohy <76951555+Dohy-Lee@users.noreply.github.com>
2026-07-28 13:30:26 +0900

    Add files via upload

--- Yozm_ai_agent/ch5/loop_agent/agent.py
+++ Yozm_ai_agent/ch5/loop_agent/agent.py
@@ -14,16 +14,16 @@ random_generator = LlmAgent(
 spam_checker = LlmAgent(
     name='SpamChecker',
     model='gemini-3.1-flash-lite-preview',
-    instruction='사용자의 입력이 스팸인지 확인하세요. 스팸이면 "Fail", 아니면 "Pass"를 반환하세요.',
+    instruction='{random_message}이 스팸인지 확인하세요. 스팸이면 "Fail", 아니면 "Pass"를 반환하세요.',
     output_key='spam_status'
 )
 
 # 상태를 확인하고, 정상적인 메세지면 루프를 중단하도록 요청하는 커스텀 에이전트
 class CheckStatusAndEscalate(BaseAgent):
     async def _run_async_impl(
-            self, ctx: InvocationContext
-    ) -> AsyncGenerator[Event, None]:
-        status = ctx.session.state.get("spam_status",'Fail')
+            self, ctx: InvocationContext # InvocationContext: 이번 실행에 대한 모든 정보를 저장한 객체
+    ) -> AsyncGenerator[Event, None]: # Event: 이 함수가 yield로 내보내는 것, None: 밖에서 이 Generator로 보낼 수 있는 값
+        status = ctx.session.state.get("spam_status",'Fail') # get의 두 번쨰 인자(여기선 Fail)은 디폴트값
         should_stop = status == "Pass" # Pass 상태면 루프 중지
         yield Event(author=self.name, actions=EventActions(escalate=should_stop))
 
